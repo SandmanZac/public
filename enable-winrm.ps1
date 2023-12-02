9fd0b29218b08f438928a40fd6dfa01d5cca5e31
SandmanZac <107512360+SandmanZac@users.noreply.github.com>
2023-12-02 10:13:33 -0600

    Update enable-winrm.ps1

--- enable-winrm.ps1
+++ enable-winrm.ps1
@@ -15,6 +15,6 @@ $connections | ForEach-Object {
 }
 
 Restart-Service -Name WinRM
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1' -UseBasicParsing -Outfile $env:TEMP\ConfigureRemotingForAnsible.ps1
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/SandmanZac/public/main/ConfigureRemotingForAnsible.ps1' -UseBasicParsing -Outfile $env:TEMP\ConfigureRemotingForAnsible.ps1
 
 . "$($env:TEMP)\ConfigureRemotingForAnsible.ps1" -CertValidityDays 3650

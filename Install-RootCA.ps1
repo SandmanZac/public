7a6bd2c3e950fe57084a58297553356143007ff8
SandmanZac <107512360+SandmanZac@users.noreply.github.com>
2023-12-11 14:16:50 -0600

    Update Install-RootCA.ps1

--- Install-RootCA.ps1
+++ Install-RootCA.ps1
@@ -1,7 +1,7 @@
 Add-WindowsFeature Adcs-Cert-Authority -IncludeManagementTools
-Install-AdcsCertificationAuthority –CAType StandaloneRootCA –CACommonName "CAZ Offline Root CA" –KeyLength 4096 –HashAlgorithm SHA512 –CryptoProviderName "RSA#Microsoft Software Key Storage Provider"
+Install-AdcsCertificationAuthority -CAType StandaloneRootCA -CACommonName "CAZ Offline Root CA" -KeyLength 4096 -HashAlgorithm SHA512 -CryptoProviderName "RSA#Microsoft Software Key Storage Provider"
 $aialist = Get-CAAuthorityInformationAccess; foreach ($aia in $aialist) {Remove-CAAuthorityInformationAccess $aia.uri -Force};
-Add-CAAuthorityInformationAccess –AddToCertificateAia -uri http://pki.caz.one/pki
+Add-CAAuthorityInformationAccess -AddToCertificateAia -uri http://pki.caz.one/pki
 $crllist = Get-CACrlDistributionPoint; foreach ($crl in $crllist) {Remove-CACrlDistributionPoint $crl.uri -Force};
 Add-CACRLDistributionPoint -Uri C:\Windows\System32\CertSrv\CertEnroll\%3%8.crl -PublishToServer -Force
 Add-CACRLDistributionPoint -Uri http://pki.caz.one/pki/%3%8.crl -AddToCertificateCDP -Force

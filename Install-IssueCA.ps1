583c49470a9b07b6bea29e8a2899d4aac516ab34
SandmanZac <107512360+SandmanZac@users.noreply.github.com>
2023-12-11 14:33:21 -0600

    Update Install-IssueCA.ps1

--- Install-IssueCA.ps1
+++ Install-IssueCA.ps1
@@ -1,3 +1,5 @@
+cd c:\installs
 certutil -dspublish -f "offlineCA_CAZ Offline Root CA.crt" RootCA
 certutil -addstore -f root "offlineCA_CAZ Offline Root CA.crt"
 certutil -addstore -f root "CAZ Offline Root CA.crl"
+certutil -dspublish "CAZ Offline Root CA.crl"

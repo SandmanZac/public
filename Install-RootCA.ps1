74e25485734a24f3a86737f23cdbab177e4cb750
SandmanZac <107512360+SandmanZac@users.noreply.github.com>
2023-12-11 15:06:17 -0600

    Update Install-RootCA.ps1

--- Install-RootCA.ps1
+++ Install-RootCA.ps1
@@ -1,10 +1,10 @@
 Add-WindowsFeature Adcs-Cert-Authority -IncludeManagementTools
-Install-AdcsCertificationAuthority -CAType StandaloneRootCA -CACommonName "CAZ Offline Root CA" -KeyLength 4096 -HashAlgorithm SHA512 -CryptoProviderName "RSA#Microsoft Software Key Storage Provider"
-$aialist = Get-CAAuthorityInformationAccess; foreach ($aia in $aialist) {Remove-CAAuthorityInformationAccess $aia.uri -Force};
+Install-AdcsCertificationAuthority -CAType StandaloneRootCA -CACommonName "CAZ Offline Root CA" -KeyLength 4096 -HashAlgorithm SHA512 -CryptoProviderName "RSA#Microsoft Software Key Storage Provider" -Force -Confirm:$false
+$aialist = Get-CAAuthorityInformationAccess; foreach ($aia in $aialist) {Remove-CAAuthorityInformationAccess $aia.uri -Force -Confirm:$false};
 Add-CAAuthorityInformationAccess -AddToCertificateAia -uri http://pki.caz.one/pki
-$crllist = Get-CACrlDistributionPoint; foreach ($crl in $crllist) {Remove-CACrlDistributionPoint $crl.uri -Force};
-Add-CACRLDistributionPoint -Uri C:\Windows\System32\CertSrv\CertEnroll\%3%8.crl -PublishToServer -Force
-Add-CACRLDistributionPoint -Uri http://pki.caz.one/pki/%3%8.crl -AddToCertificateCDP -Force
+$crllist = Get-CACrlDistributionPoint; foreach ($crl in $crllist) {Remove-CACrlDistributionPoint $crl.uri -Force -Confirm:$false};
+Add-CACRLDistributionPoint -Uri C:\Windows\System32\CertSrv\CertEnroll\%3%8.crl -PublishToServer -Force -Confirm:$false
+Add-CACRLDistributionPoint -Uri http://pki.caz.one/pki/%3%8.crl -AddToCertificateCDP -Force -Confirm:$false
 Certutil -setreg CA\CRLOverlapPeriodUnits 24
 Certutil -setreg CA\CRLOverlapPeriod "Hours"
 Certutil -setreg CA\ValidityPeriodUnits 5

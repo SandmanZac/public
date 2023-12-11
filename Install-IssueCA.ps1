bba98d211cef3371d79da07a507189b4978a22d9
SandmanZac <107512360+SandmanZac@users.noreply.github.com>
2023-12-11 14:30:08 -0600

    Update Install-IssueCA.ps1

--- Install-IssueCA.ps1
+++ Install-IssueCA.ps1
@@ -1,3 +1,3 @@
-certutil –dspublish –f "CAZ Offline Trusted Root.crt" RootCA
-certutil –addstore –f root orca1_ContosoRootCA.crt
-certutil –addstore –f root ContosoRootCA.crl
+certutil –dspublish –f "offlineCA_CAZ Offline Root CA.crt" RootCA
+certutil –addstore –f root "offlineCA_CAZ Offline Root CA.crt"
+certutil –addstore –f root "CAZ Offline Root CA.crl"

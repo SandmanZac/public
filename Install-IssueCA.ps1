4315e5a22e9c508504198431bae61887647a5bb6
SandmanZac <107512360+SandmanZac@users.noreply.github.com>
2023-12-11 15:44:24 -0600

    Update Install-IssueCA.ps1

--- Install-IssueCA.ps1
+++ Install-IssueCA.ps1
@@ -3,3 +3,4 @@ certutil -dspublish -f "offlineCA_CAZ Offline Root CA.crt" RootCA
 certutil -addstore -f root "offlineCA_CAZ Offline Root CA.crt"
 certutil -addstore -f root "CAZ Offline Root CA.crl"
 certutil -dspublish "CAZ Offline Root CA.crl"
+Install-AdcsCertificationAuthority –CAType EnterpriseSubordinateCA –CACommonName "CAZ Issue CA" –KeyLength 4096 –HashAlgorithm SHA512 –CryptoProviderName "RSA#Microsoft Software Key Storage Provider"
